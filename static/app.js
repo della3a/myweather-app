ce2ad33cd53fe04b7699f743ae034563eaed22b0
della3a <manelkheffache@gmail.com>
2022-08-15 10:31:26 +0200

    basic weather app  v2 that works

--- static/app.js
+++ static/app.js
@@ -10,8 +10,6 @@
 };
 
 let weather ={
-
-	apikey : '421ee44f7cmsh4cdbcb5aecb032ep198612jsnff7eda551739',
 	fetchWeather : function(city){
 		fetch('https://weatherapi-com.p.rapidapi.com/current.json?q='+city, options)
 		.then(response => response.json())
@@ -21,16 +19,13 @@ let weather ={
 	},
 
 	displayWeather : function(data){
-		const { name } = data.location;
-		const {localtime} = data.location;
-		const { temp_c } = data.current;
-		const { clouds } = data.current;
-		const { humidity } = data.current;
-		const { wind_kph } = data.current;
+		const { name, country, localtime } = data.location;
+		const { temp_c, clouds, humidity, wind_kph } = data.current;
 
-		console.log(`${name} ${localtime} ${temp_c} ${humidity} ${wind_kph}`);
+		console.log(`${name} ${country} ${localtime} ${temp_c} ${humidity} ${wind_kph}`);
 
 		document.querySelector('.city').innerText = "Weather in " + name;
+    document.querySelector('.country').innerText = country;
     document.querySelector('.time').innerText = localtime;
 		document.querySelector('.temp').innerText = temp_c + "°C";
 		document.querySelector('.clouds').innerText = "Clouds: " + clouds + "%";
@@ -42,7 +37,7 @@ let weather ={
 
 setInterval(() => {
   let city = cities[Math.floor(Math.random() * cities.length)];
-  let temp = document.querySelector('.temp');  
+  weather.fetchWeather(city.name);
 }, 3000);
 
 
